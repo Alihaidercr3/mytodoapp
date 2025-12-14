35b52a41099413ec2586e06200d0526d4e9b34f2
alihaidercr3@gmail.com <alihaidercr3@gmail.com>
2025-12-14 21:55:27 +0500

    finally

--- src/pages/Authentication/index.jsx
+++ src/pages/Authentication/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Login from './Login.jsx'   
+import Login from './login.jsx'   
 import Register from './Register.jsx'
 export default function index() {
   return (
